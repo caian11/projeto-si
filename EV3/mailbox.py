f653fd43bb56ecc3fb02b0e20333b1bed6c6902b
Jerry Nicholls <Jerry.Nicholls@zeninternet.co.uk>
2019-11-13 13:00:47 +0000

    Made the type a nested class enum.

--- EV3/mailbox.py
+++ EV3/mailbox.py
@@ -1,8 +1,12 @@
 #!/usr/bin/env python3
 import struct
+from enum import Enum
 
 class Mailbox:
-    BOOL, NUMBER, TEXT = range(3)
+    class Type(Enum):
+        BOOL   = 1
+        NUMBER = 2
+        TEXT   = 3
 
     headerBytes = '\x01\x00\x82\x9e'.encode('latin-1')
 
@@ -13,13 +17,13 @@ class Mailbox:
         nameBytes = (name + '\x00').encode('latin-1')
         nameLen   = len(nameBytes)
 
-        if type == Mailbox.BOOL:
+        if type == Mailbox.Type.BOOL:
             contentBytes = struct.pack('B', 1 if value == True else 0)
 
-        if type == Mailbox.NUMBER:
+        if type == Mailbox.Type.NUMBER:
             contentBytes = struct.pack('f',float(value))
 
-        if type == Mailbox.TEXT:
+        if type == Mailbox.Type.TEXT:
             contentBytes = (value + '\x00').encode('latin-1')
 
         contentLen = len(contentBytes)
@@ -35,7 +39,7 @@ class Mailbox:
         ))
 
 if __name__ == '__main__':
-    print(Mailbox.encode("string","mortimer",Mailbox.TEXT))
-    print(Mailbox.encode("true",True,Mailbox.BOOL))
-    print(Mailbox.encode("false",True,Mailbox.BOOL))
-    print(Mailbox.encode("number",3.141,Mailbox.NUMBER))
+    print(Mailbox.encode("string","mortimer",Mailbox.Type.TEXT))
+    print(Mailbox.encode("true",True,Mailbox.Type.BOOL))
+    print(Mailbox.encode("false",True,Mailbox.Type.BOOL))
+    print(Mailbox.encode("number",3.141,Mailbox.Type.NUMBER))
