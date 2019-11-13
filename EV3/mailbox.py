666e2a9dd7fa73db0bcea8cc0ce18f4b7e494a59
Jerry Nicholls <Jerry.Nicholls@zeninternet.co.uk>
2019-11-13 12:54:07 +0000

    Lunchtime crack at getting the binary format sorted.

--- EV3/mailbox.py
+++ EV3/mailbox.py
@@ -1,38 +1,41 @@
 #!/usr/bin/env python3
 import struct
-from enum import Enum
-
-class MailboxType(Enum):
-    BOOL   = 0
-    NUMBER = 1
-    TEXT    = 2
 
 class Mailbox:
+    BOOL, NUMBER, TEXT = range(3)
+
     headerBytes = '\x01\x00\x82\x9e'.encode('latin-1')
 
     def raw_bytes(data):
         return " ".join("{:02x}".format(c) for c in data)
 
     def encode(name, value, type):
-        name    = name + '\x00'
-        nameLen = len(name)
-        packFmt = '<H4BB{}B'.format(nameLen)
+        nameBytes = (name + '\x00').encode('latin-1')
+        nameLen   = len(nameBytes)
 
-        if type == MailboxType.BOOL:
-            content = 1 if value == True else 0
-            packFmt = packFmt + '<HB'
+        if type == Mailbox.BOOL:
+            contentBytes = struct.pack('B', 1 if value == True else 0)
 
-        if type == MailboxType.NUMBER:
-            content = float(value)
-            packFmt = packFmt + '<Hf'
+        if type == Mailbox.NUMBER:
+            contentBytes = struct.pack('f',float(value))
 
-        if type == MailboxType.TEXT:
-            value   = value + '\x00'
-            content = value.encode('latin-1')
-            packFmt = packFmt + '<H{}B'.format(len(content))
+        if type == Mailbox.TEXT:
+            contentBytes = (value + '\x00').encode('latin-1')
 
-        print(packFmt)
-            
+        contentLen = len(contentBytes)
+
+        # 4ByteHeader + NameLenByte + Name + ContentLen2Bytes + Content
+        totalLen   = nameLen + contentLen + 7
+
+        return(struct.pack(
+            '<H4sB{}sH{}s'.format(nameLen,contentLen),
+            totalLen, Mailbox.headerBytes,
+            nameLen, nameBytes,
+            contentLen, contentBytes
+        ))
 
 if __name__ == '__main__':
-    Mailbox.encode("bob","mortimer",MailboxType.TEXT)
+    print(Mailbox.encode("string","mortimer",Mailbox.TEXT))
+    print(Mailbox.encode("true",True,Mailbox.BOOL))
+    print(Mailbox.encode("false",True,Mailbox.BOOL))
+    print(Mailbox.encode("number",3.141,Mailbox.NUMBER))
